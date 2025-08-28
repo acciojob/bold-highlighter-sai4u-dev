bcc9b158d086ce5b0810111fa8d1ba106a2649a8
Sai Narendra <tannirusainarendra81@gmail.com>
2025-08-29 00:58:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 function highlight() {
-    //Write your code here
-
-
+    const strongTags = document.querySelectorAll('strong');
+    strongTags.forEach(tag => {
+        tag.style.color = 'rgb(0, 128, 0)'; // Green
+    });
 }
 
-
 function return_normal() {
-    //Write your code here
-
-    
+    const strongTags = document.querySelectorAll('strong');
+    strongTags.forEach(tag => {
+        tag.style.color = 'rgb(0, 0, 0)'; // Black
+    });
 }
